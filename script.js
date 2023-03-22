7e2c7e0967d9c6e4bd57f62bbd6294abc100b093
NicoGerbaudo <nicogerbaudo2000ggmail.com>
2023-03-22 19:59:55 -0300

    Se agrega proyecto

---
+++ script.js
@@ -0,0 +1,56 @@
+let sections = document.querySelectorAll('section')
+let navLinks = document.querySelectorAll('header nav a')
+
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+let header = document.querySelector('.header');
+
+header.classList.toggle('sticky', window.scrollY > 100);
+}
+
+
+const $form = document.querySelector('#form')
+
+
+$form.addEventListener('submit', handleSubmit)
+
+async function handleSubmit(event) {
+    event.preventDefault()
+    const form = new FormData(this)
+    const response = await fetch(this.action, {
+        method: this.method,
+        body: form,
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    if (response.ok) {
+        this.reset()
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Thanks for contacting me!',
+            text: ' I would be writing to you soon!' ,
+            showConfirmButton: true,
+            timer: 3500
+          })
+    }
+}
+
+
+
+
